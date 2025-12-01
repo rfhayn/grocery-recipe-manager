16bb6e2c2f5e666099b17d2337e8147e981dc567
Rich Hayn <richard.hayn@gmail.com>
2025-12-01 06:27:39 -0500

    M5.0.3B COMPLETE: Core Data model renamed to forager - data persists

--- GroceryRecipeManager/forager/Persistence.swift
+++ GroceryRecipeManager/forager/Persistence.swift
@@ -414,7 +414,7 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "GroceryRecipeManager")
+        container = NSPersistentContainer(name: "forager")  
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
